072cd2ff94dda4e69bbb27ecfa2154b6d055f5a6
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-05-14 10:39:09 +0200

    initial commit (mirror from private gitlab without history)

---
+++ README.md
@@ -0,0 +1,6 @@
+> [!NOTE]
+> Mirror from: https://scm.medienhaus.udk-berlin.de/andirueckel/peertube-theme-medienhaus
+
+## Contributors / Credits
+- Frederic Brodbeck — GitHub: [@freder](https://github.com/freder)
+- Max Wo — GitHub: [@Franz-Kafka](https://github.com/Franz-Kafka)
---
+++ client/video-watch-client-plugin.js
@@ -0,0 +1,32 @@
+function register ({ registerHook, peertubeHelpers }) {
+  
+  /*
+  document.querySelectorAll(".menu-wrapper").forEach(el => el.remove());
+  
+  var mainContent = document.querySelector("#content")
+  mainContent.classList.add("expanded");
+  
+  
+
+    var isPresentingVideo = document.querySelector("#video-wrapper");
+    console.log(isPresentingVideo)
+    if(isPresentingVideo){
+      console.log('Videoplayer')
+    }
+
+  */
+  
+  /*
+  var isPresentingVideo = $('#video-wrapper');
+  
+  if(isPresentingVideo){
+    $('#content').addClass('expanded');
+    $('.menu-wrapper').remove();
+  }
+  */
+  
+}
+
+export {
+  register
+}
